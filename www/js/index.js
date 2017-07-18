65bd494a2341ab8fab396ce380cc25bf6c687845
Dave Alden <dpa99c@gmail.com>
2017-07-18 10:17:31 +0100

    Remove cordova-plugin-popupbridge which adds PopupBridge to Android System Webview (which is not used).

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,29 @@
 var URL = "https://braintree.github.io/popup-bridge-example/";
 
-var webView;
+var webView, platform, iabOpts;
 if( navigator.platform.substr(0,2) === 'iP' ) {    // iOS detected
+    platform = "iOS";
+    iabOpts = 'location=no,toolbar=yes';
     if( window.webkit && window.webkit.messageHandlers ) {
         webView = "WKWebView" ;
     }else{
         webView = "UIWebView" ;
     }
 }else{
+    platform = "Android";
     webView = "Chromium" ;
+    iabOpts = 'location=yes';
 }
 
 
 function openIAB(){
-    cordova.InAppBrowser.open(URL, '_blank', 'location=no,toolbar=yes');
+    cordova.InAppBrowser.open(URL, '_blank', iabOpts);
 }
 
 function onDeviceReady(){
     console.log("deviceready");
+    console.log("PopupBridge present on Cordova Webview: " + window.popupBridge);
+    $('#platform').html(platform);
     $('#webview').html(webView);
 }
 
